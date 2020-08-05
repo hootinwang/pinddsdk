852d082c61d41332bfffa76ecd60f48bf01e0b99
wanghaotian <123456>
2020-08-05 15:12:58 +0800

    整合基础结构

---
+++ src/Exceptions/PinduoduoException.php
@@ -0,0 +1,14 @@
+<?php
+namespace Pinduoduo\Exceptions;
+/**
+ * Created by PhpStorm.
+ * User: wuyan
+ * Date: 2020/8/5
+ * Time: 15:10
+ */
+class PinduoduoException extends \Exception
+{
+    function __construct($message) {
+        parent::__construct($message);
+    }
+}
---
+++ src/Test.php
@@ -0,0 +1,12 @@
+<?php
+namespace Pinduoduo;
+/**
+ * Created by PhpStorm.
+ * User: wuyan
+ * Date: 2020/8/5
+ * Time: 15:08
+ */
+class Test
+{
+
+}
